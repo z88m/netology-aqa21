2860898b4d9695d80575412a8e44b18aa1637a4d
z88m <a708yx@gmail.com>
2019-11-06 19:23:10 +0300

    rebuild 1st positive test. Now it works!

--- src/test/java/ru/netology/web/Aqa21Test.java
+++ src/test/java/ru/netology/web/Aqa21Test.java
@@ -2,24 +2,22 @@ package ru.netology.web;
 
 import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 
-import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class Aqa21Test {
 
-    private WebDeiver driver;
-
     @Test
     void shouldTest() {
         open("http://localhost:9999");
-        SelenideElement form = $("[data-test-id=callback-form]");
-        form.$("[data-test-id=name] input").setValue("Василий Алибабаевич");
-        form.$("[data-test-id=phone] input").setValue("+71234567890");
-        form.$("[data-test-id=agreement]").click();
 
-        form.$("[data-test-id=submit]").click();
-        $(".alert-success").shouldHave(exactText("Ваша заявка успешно отправлена!"));
+        $("[data-test-id=name] input").setValue("Василий Алибабаевич");
+        $("[data-test-id=phone] input").setValue("+71234567890");
+        $("[data-test-id=agreement]").click();
+        $(By.tagName("button")).click();
+        $("[data-test-id=order-success]").should(exist);
     }
 }
